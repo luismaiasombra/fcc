eba521cdcfdb11d12e98bea106566e1a1a043fe3
luismaiasombra <95807950+luismaiasombra@users.noreply.github.com>
2023-03-21 17:37:39 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const express = require("express")
+const app = express();
+/*
+
+
+app.get('/:word/echo', (req, res) => {
+  const word = req.params.word;
+  res.json({ echo: word });
+});
+
+;*/
+app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.json({
+    echo: word
+  });
+});
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+})
