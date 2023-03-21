b2f26d7615f4163dc24bdd52d314a7c3500e0a72
luismaiasombra <95807950+luismaiasombra@users.noreply.github.com>
2023-03-21 18:03:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,7 @@ const hostname = '127.0.0.1';
 const port = 3000;
 const express = require("express")
 const app = express();
-/*
 
-
-app.get('/:word/echo', (req, res) => {
-  const word = req.params.word;
-  res.json({ echo: word });
-});
-
-;*/
 app.get("/:word/echo", (req, res) => {
   const word = req.params.word;
   res.json({
